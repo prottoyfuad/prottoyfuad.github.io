311faa981d7d3f63f99f2168f5c7c2c4e21cb9cf
prottoyfuad <prottoyfuad@gmail.com>
2025-02-06 02:09:31 +0600

    Added Home Page with Old Blog (Flow)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { marked } from 'marked';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  site: any = {
+    title: 'Eucalyptus',
+    logo: './logo.jpg',
+    url: 'https://prottoyfuad.github.io',
+    description: [
+      'ছায়ারো ছায়াতে সে অন্যজন,',
+      'ভরদুপুরে একলা রাতে অন্যমন!'
+    ],
+    userInfo: [
+      'Prottoy Fuad',
+      'SWE | Competitive Programmer',
+      'Chittagong, Bangladesh'
+    ]
+  }
+
+  git: any = {
+    username: 'prottoyfuad',
+    fullName: 'Prottoy Fuad',
+    url: 'https://github.com/prottoyfuad',
+    repo: 'prottoyfuad',
+    repoUrl: 'https://github.com/prottoyfuad/prottoyfuad/tree/angular',
+    nwo: 'prottoyfuad/prottoyfuad'
+  }
+
+  markdownContent: string = "";
+  htmlContent: string = "";
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.httpClient.get('blogs/flow.md', { responseType: 'text' })
+      .subscribe({
+        next: (markdown: string) => {
+          this.markdownContent = markdown; // Store raw markdown content
+          this.htmlContent = marked(markdown).toString(); // Convert Markdown to HTML
+        },
+        error: (err) => {
+          console.error('Error loading Markdown file', err);
+        }
+      });
+  }
+  
 }
